4cfce8b0cca4c50175de996a25f5268da0478880
mpekchri <mpekchri@auth.gr>
2017-07-18 16:01:18 +0300

    Update test.m

--- test.m
+++ test.m
@@ -4,14 +4,16 @@ xb = l.x;                       % xb = our bayer layer
 [M,N] = size(xb);               % desired output dimensions equal to xb's dimensions
 
 %% use nearest method as sampling method and show image
+M = M/2;
+N = N/2;
 im = nearest_Rule_KA1(xb,M,N);
 figure
 imshow(im)
 title('rule KA1 , rgb dimensions == bayer layer dimensions')
 
 %% use nearest method as sampling method and show image
-M = 2*M;
-N = 2*N;                        % desired output dimensions > xb's dimensions
+M = 4*M;
+N = 4*N;                        % desired output dimensions > xb's dimensions
 im = nearest_Rule_KA2(xb,M,N);
 figure
 imshow(im)
