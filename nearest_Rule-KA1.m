f2b52ffc4673efebca400228432648690e26da79
mpekchri <mpekchri@auth.gr>
2017-07-18 16:00:18 +0300

    Works properly only for desired output dimensions M<Mo and N<No (Mo,No = bayer layer dimensions)

--- nearest_Rule-KA1.m
+++ nearest_Rule-KA1.m
@@ -5,87 +5,80 @@ function results = nearest_Rule_KA1(xb,M,N)
 % M = number of rows , N = number of columns
 %% outputs
 % results = rgb image with M rows and N columns
-Mo = size(xb,1);
-No = size(xb,2);
-results = zeros(Mo,No,3);
+results = zeros(M,N,3);
 
-%% Additional informations
-% according to the inputs , we will create the result by applying nearest algorithm
-% in this case , the value of each triad (of output image) is determined by only one other cell of the xb (bayer layer)
-% More info about this method (it's really not necessary) can be found in report.pdf
+%% indexing according to rule_KA1
+% indexes will be like : 
+% I = ((i-1)+1/2)*(size(xb,1)/M);
+% J = ((j-1)+1/2)*(size(xb,2)/N);
+b_y = size(xb,1)/M;
+b_x = size(xb,2)/N;
+w = [0 0];
 
-%% indexing preparations
-% we set the indexes the way that rule [KA2] (mentioned in report2.pdf) describes them .
-% In order to avoid exceeding xb dimensions we apply padding to xb
-% so the final indexes will be I=I+1 , J = J+1
-xb = padarray(xb,[1 1],'symmetric','both');
-
-% b_y = Mo/M and b_x = No/N
-b_y = M/Mo;
-b_x = N/No;
+%% padding in order to avoid (0,0) cell etc...
+% we should now "transpose" our coordinates according to : i+1 ,j+0
+x = padarray(xb,[2 2],'symmetric','both');
 
 %% Bayer Pattern
-% what about if we want to implement another bayer pattern,
-% instead of 'grbg' ? we will add some weight to our indexes, 
-% like I = I + w(1) , J = J + w(2)
-% if w=[0 0] we got a solution for grbg bayer image
-% if w=[1 0] we got a solution for bggr bayer image
-% if w=[0 1] we got a solution for rggb bayer image
-% if w=[1 1] we got a solution for gbrg bayer image
-w = [0 0];
-wj = w(2);
-wi = w(1);
+% in this soluction we assume that our bayer image pattern is rggb
+% actually we create the matlab function demosaic with input 'rggb'
+% Just for demonstrating reasons i will add some variables,that will give us
+% the oportunity to result demosaic output when given strign is 'gbrg' or
+% ...
 
-%% case green - (the first/more-left green in a grbg patern)
-lj = [1 No-1];
-li = [1 Mo-1];
-lj = lj+1+wj;
-li = li+1+wi;
+% in order to run function in mode rggb : wi = 1 , wj = 0
+% in order to run function in mode gbrg : wi = 0 , wj = 1
+% in order to run function in mode grbg : wi = 1 , wj = 1
+% in order to run function in mode bggr : wi = 0 , wj = 0
+% grbg select
+wj = 1;
+wi = 1;
 
-results(li(1):2:li(2),lj(1):2:lj(2),1) = xb(li(1):2:li(2),lj(1)+1:2:lj(2)+1);%red
-results(li(1):2:li(2),lj(1):2:lj(2),2) = xb(li(1):2:li(2),lj(1):2:lj(2)); %green
-results(li(1):2:li(2),lj(1):2:lj(2),3) = xb(li(1)+1:2:li(2)+1,lj(1):2:lj(2)); %blue
+%% general case
+% i for y, j for x
+% lj = [j_start j_end];
+% li = [i_start -i_end];
 
-%% case red
-lj = [2 No];
-li = [1 Mo-1];
-lj = lj+1+wj;
-li = li+1+wi;
-results(li(1):2:li(2),lj(1):2:lj(2),1) = xb(li(1):2:li(2),lj(1):2:lj(2)); %red
-results(li(1):2:li(2),lj(1):2:lj(2),2) = xb(li(1)+1:2:li(2)+1,lj(1):2:lj(2));
-results(li(1):2:li(2),lj(1):2:lj(2),3) = xb(li(1)+1:2:li(2)+1,lj(1)-1:2:lj(2)-1);
+% case blue
+lj = [2 N];
+li = [2 M];
+lI = (li -1/2+w(1))*b_y+1+wi;
+lJ = (lj - 1/2+w(1))*b_x+1+wj;
 
-%% case blue
-lj = [1 No-1];
-li = [2 Mo];
-lj = lj+1+wj;
-li = li+1+wi;
-results(li(1):2:li(2),lj(1):2:lj(2),1) = xb(li(1)+1:2:li(2)+1,lj(1)+1:2:lj(2)+1); %red
-results(li(1):2:li(2),lj(1):2:lj(2),2) = xb(li(1)+1:2:li(2)+1,lj(1):2:lj(2));
-results(li(1):2:li(2),lj(1):2:lj(2),3) = xb(li(1):2:li(2),lj(1):2:lj(2));
+results(li(1):1:li(2),lj(1):1:lj(2),1) = x(lI(1)+1:b_y:lI(2)+1,lJ(1)-1:b_x:lJ(2)-1) ;%red;
+results(li(1):1:li(2),lj(1):1:lj(2),2) = x(lI(1)+1:b_y:lI(2)+1,lJ(1):b_x:lJ(2)); %green
+results(li(1):1:li(2),lj(1):1:lj(2),3) = x(lI(1):b_y:lI(2),lJ(1):b_x:lJ(2)); %blue
 
-%% case green -(the second/more-right green in a grbg patern)
-lj = [2 No];
-li = [2 Mo];
-lj = lj+1+wj;
-li = li+1+wi;
-results(li(1):2:li(2),lj(1):2:lj(2),1) = xb(li(1)+1:2:li(2)+1,lj(1):2:lj(2)); %red
-results(li(1):2:li(2),lj(1):2:lj(2),2) = xb(li(1):2:li(2),lj(1):2:lj(2));
-results(li(1):2:li(2),lj(1):2:lj(2),3) = xb(li(1):2:li(2),lj(1)-1:2:lj(2)-1);
+%case red
+lj = [1 N-1];
+li = [1 M-1];
+lI = (li -1/2-w(1))*b_y+1+wi;
+lJ = (lj - 1/2-w(1))*b_x+1+wj;
 
-grid_x = linspace(1,No,N);
-grid_y = linspace(1,Mo,M);
-grid_x = floor(grid_x);
-grid_y = floor(grid_y);
+results(li(1):1:li(2),lj(1):1:lj(2),1) = x(lI(1):b_y:lI(2),lJ(1):b_x:lJ(2));%red;
+results(li(1):1:li(2),lj(1):1:lj(2),2) = x(lI(1)+1:b_y:lI(2)+1,lJ(1):b_x:lJ(2)); %green
+results(li(1):1:li(2),lj(1):1:lj(2),3) = x(lI(1)+1:b_y:lI(2)+1,lJ(1)+1:b_x:lJ(2)+1); %blue
 
-finalres = zeros(M,N,3);
-for i=1:b_y:M
-    for j=1:b_x:N
-        finalres(i:i+b_y,j:j+b_x,1) = results(grid_y(i),grid_x(i),1);
-        finalres(i:i+b_y,j:j+b_x,2) = results(grid_y(i),grid_x(i),2);
-        finalres(i:i+b_y,j:j+b_x,3) = results(grid_y(i),grid_x(i),3);
-    end
-end
-results = finalres;
+%case green
+%(case green 1: i=2:2:M-2,j=3:2:N-1)
+lj = [1 N-1];
+li = [2 M];
+lI = (li -1/2+w(1))*b_y+1+wi;
+lJ = (lj - 1/2+w(1))*b_x+1+wj;
+
+results(li(1):1:li(2),lj(1):1:lj(2),1) = x(lI(1)+1:b_y:lI(2)+1,lJ(1):b_x:lJ(2));%red;
+results(li(1):1:li(2),lj(1):1:lj(2),2) = x(lI(1):b_y:lI(2),lJ(1):b_x:lJ(2));  %green
+results(li(1):1:li(2),lj(1):1:lj(2),3) = x(lI(1):b_y:lI(2),lJ(1)-1:b_x:lJ(2)-1); %blue
+
+%(case green 2: i=3:2:M-1,j=2:2:N-2)
+lj = [2 N];
+li = [1 M-1];
+lI = (li -1/2+w(1))*b_y+1+wi;
+lJ = (lj - 1/2+w(1))*b_x+1+wj;
+
+results(li(1):1:li(2),lj(1):1:lj(2),1) = x(lI(1):b_y:lI(2),lJ(1):b_x:lJ(2));%red
+results(li(1):1:li(2),lj(1):1:lj(2),2) = x(lI(1):b_y:lI(2),lJ(1)-1:b_x:lJ(2)-1); %green
+results(li(1):1:li(2),lj(1):1:lj(2),3) = x(lI(1)+1:b_y:lI(2)+1,lJ(1):b_x:lJ(2));%blue
 
 end
+
